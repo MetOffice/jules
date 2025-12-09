d7944b607f528759f96051e9e13389739444c3a1
James Bruten <109733895+james-bruten-mo@users.noreply.github.com>
2025-12-09 14:42:06 +0000

    vn8.0 pre-release (#13)
    Co-authored-by: Jenny Hickson <61183013+jennyhickson@users.noreply.github.com>

--- rose-meta/jules-standalone/versions.py
+++ rose-meta/jules-standalone/versions.py
@@ -40,76 +40,17 @@ from .version75_76 import *
 from .version76_77 import *
 from .version77_78 import *
 from .version78_79 import *
+from .version79_80 import *
 
+class vnYY_txxxx(MacroUpgrade):
 
-class vn79_t1460(MacroUpgrade):
-    """Upgrade macro from JULES by Maggie Hendry"""
+    """Upgrade macro from JULES by Author"""
 
-    BEFORE_TAG = "vn7.9"
-    AFTER_TAG = "vn7.9_t1460"
-
-    def upgrade(self, config, meta_config=None):
-        """Upgrade a JULES runtime app configuration."""
-        # Remove redundant switch as River routing ancillaries should now be
-        # consistent.
-        self.remove_setting(
-            config,
-            ["namelist:jules_rivers_props", "l_ignore_ancil_rivers_check"],
-        )
-
-        return config, self.reports
-
-
-class vn79_t1392(MacroUpgrade):
-    """Upgrade macro from JULES by Maggie Hendry"""
-
-    BEFORE_TAG = "vn7.9_t1460"
-    AFTER_TAG = "vn7.9_t1392"
+    BEFORE_TAG = "vnY.Y"
+    AFTER_TAG = "vnY.Y_txxxx"
 
     def upgrade(self, config, meta_config=None):
         """Upgrade a JULES runtime app configuration."""
-        # Add is_climatology flag to jules_rivers_props
-        nvars = int(
-            self.get_setting_value(
-                config, ["namelist:jules_rivers_props", "nvars"]
-            )
-        )
-        self.add_setting(
-            config,
-            ["namelist:jules_rivers_props", "is_climatology"],
-            ",".join([".false."] * nvars),
-        )
-        return config, self.reports
-
-
-class vn79_t1371(MacroUpgrade):
-
-    """ Upgrade macro from JULES by Katty Huang """
-
-    BEFORE_TAG = "vn7.9_t1392"
-    AFTER_TAG = "vn7.9_t1371"
-
-    def upgrade(self,config, meta_config=None):
-        """Upgrade a JULES runtime app configuration."""
 
         # Add settings
-        self.add_setting(config, ["namelist:jules_surface","anthrop_heat_option"], "0")
-        self.add_setting(config, ["namelist:jules_surface","anthrop_heat_mean"], "20.0")
-
-        return config, self.reports
-
-
-class vn79_t1088(MacroUpgrade):
-
-    """Upgrade macro from JULES by Douglas Clark"""
-
-    BEFORE_TAG = "vn7.9_t1371"
-    AFTER_TAG = "vn7.9_t1088"
-
-    def upgrade(self,config, meta_config=None):
-        """Upgrade a JULES runtime app configuration."""
-
-        # Add settings
-        self.add_setting(config, ["namelist:jules_water_resources", "partition_method"], "2")
-        self.add_setting(config, ["namelist:jules_water_resources", "sfc_water_factor"], "2.0")
         return config, self.reports
