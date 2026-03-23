e706f5f939879d30d8ba8a7ca0fad111be243c5b
Caro.Duran.Rojas <36041957+caroduro@users.noreply.github.com>
2026-03-23 12:57:03 +0000

    Fix typo in jules_soil_biogeochem import

--- src/initialisation/standalone/init_soil_biogeochem_mod.F90
+++ src/initialisation/standalone/init_soil_biogeochem_mod.F90
@@ -41,7 +41,7 @@ USE jules_soil_biogeochem_mod, ONLY:                                           &
  ! imported scalar variables
    l_q10, soil_bgc_model, dim_ch4layer, l_ch4_tlayered,                        &
  ! imported namelists
-   jules_soil_biogeochemi,                                                     &
+   jules_soil_biogeochem,                                                     &
    l_bgc_heat
 
 USE jules_soil_mod, ONLY: sm_levels
