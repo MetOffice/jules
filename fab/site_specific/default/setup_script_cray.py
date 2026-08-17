61d3291461a5b9bd6eb2d64af732ca827eb80fe0
Joerg Henrichs <henrichsjoerg@gmail.com>
2026-08-17 16:04:30 +1000

    #102 Updated Cray flags.

--- fab/site_specific/default/setup_script_cray.py
+++ fab/site_specific/default/setup_script_cray.py
@@ -10,7 +10,10 @@ This function gets called from the default site-specific config file
 import argparse
 from typing import cast
 
-from fab.api import BuildConfig, Category, Compiler, Linker, ToolRepository
+from fab.api import (BuildConfig, Category, ContainFlags, Compiler, Linker,
+                     ToolRepository)
+
+from nf_config import NfConfig
 
 
 def setup_script_cray(build_config: BuildConfig, args: argparse.Namespace):
@@ -33,54 +36,28 @@ def setup_script_cray(build_config: BuildConfig, args: argparse.Namespace):
 
     # The base flags
     # ==============
-    flags = ["-g", "-G0", "-m", "0",    # ?
-             "-M", "E664,E7208,E7212",  # ?
-             "-en",                     # Fortran standard
-             "-ef",                     # use lowercase module names!Important!
-             "-hnocaf",                 # Required for linking with C++
-             ]
-
-    # Handle accelerator options:
-    if args.openacc or args.openmp:
-        host = args.host.lower()
-    else:
-        # Neither openacc nor openmp specified
-        host = ""
-
-    if args.openacc:
-        if host == "gpu":
-            flags.extend(["-h acc"])
-        else:
-            # CPU
-            flags.extend(["-h acc"])
-    elif args.openmp:
-        if host == "gpu":
-            flags.extend([])
-        else:
-            # OpenMP on CPU, that's already handled by Fab
-            pass
+    flags = ['-M E7208,E7212',    # Var used before defined
+             '-hlist=ad', '-hfp0', '-hflex_mp=intolerant',
+             '-dw', '-ec', '-eI', '-em', '-en']
 
     ftn.add_flags(flags, "base")
 
-    # Full debug
-    # ==========
-    ftn.add_flags(["-Ktrap=fp",    # floating point checking
-                   "-R", "bcdps",  # bounds, array shape, collapse,
-                                   # pointer, string checking
-                   "-O0"],         # No optimisation
-                  "full-debug")
-    if ftn.get_version() >= (15, 0):
-        ftn.add_flags(["-G0"], "full-debug")
-    else:
-        ftn.add_flags(["-Gfast"], "full-debug")
+    # File override to cut down compile times - compiler version: cce/12.0.1
+    if (12, 0, 1) <= ftn.get_version() < (12, 1):
+        # Setting this for all modes
+        ftn.add_flags(ContainFlags("/model_interface_mod.",
+                                   ['-O0', '-Ovector0', '-hfp0',
+                                    '-hflex_mp=strict', '-hipa0']),
+                      "base")
+        ftn.add_flags(ContainFlags("/init_prescribed_data.", ['-O1']),
+                      "base")
 
-    # Fast debug
-    # ==========
-    ftn.add_flags(["-O2"], "fast-debug")
-    if ftn.get_version() >= (15, 0):
-        ftn.add_flags(["-G2"], "fast-debug")
-    else:
-        ftn.add_flags(["-Gfast"], "fast-debug")
+    # Debug
+    # =====
+    ftn.add_flags(["-g"], "debug")
+
+    # Normal
+    # ======
 
     # Production
     # ==========
@@ -91,27 +68,9 @@ def setup_script_cray(build_config: BuildConfig, args: argparse.Namespace):
     linker = tr.get_tool(Category.LINKER, "linker-crayftn-ftn")
     linker = cast(Linker, linker)
 
-    # ATM we don't use a shell when running a tool, and as such
-    # we can't directly use "$()" as parameter. So query these values using
-    # Fab's shell tool (doesn't really matter which shell we get, so just
-    # ask for the default):
-    shell = tr.get_default(Category.SHELL)
-
-    try:
-        # We must remove the trailing new line, and create a list:
-        nc_flibs = shell.run(additional_parameters=["-c", "nf-config --flibs"],
-                             capture_output=True).strip().split()
-    except RuntimeError:
-        nc_flibs = []
-
-    linker.add_lib_flags("netcdf", nc_flibs)
-    linker.add_lib_flags("yaxt", ["-lyaxt", "-lyaxt_c"])
-    linker.add_lib_flags("xios", ["-lxios"])
-    linker.add_lib_flags("hdf5", ["-lhdf5"])
-
-    linker.add_post_lib_flags("-lcraystdc++")
-
-    # Using the GNU compiler on Crays for now needs the additional
-    # flag -fallow-argument-mismatch to compile mpi_mod.f90
-    ftn = tr.get_tool(Category.FORTRAN_COMPILER, "crayftn-gfortran")
-    ftn.add_flags("-fallow-argument-mismatch")
+    # As default, use nf-config to set NetCDF linker flags. If it's not
+    # available (or not working properly), the site-specific setup must
+    # add netcdf definitions.
+    nf_config = NfConfig()
+    if nf_config.is_available:
+        linker.add_lib_flags("netcdf", nf_config.get_linker_flags())
