3cf28bd3f0603aa5173dcd48006e0e9de9c07417
nicgedney <131251916+nicgedney@users.noreply.github.com>
2026-09-22 13:46:40 +0100

    add if not lake

--- src/science/surface/sf_evap_jls.F90
+++ src/science/surface/sf_evap_jls.F90
@@ -853,30 +853,31 @@ IF ( l_soil_evap_irrig_separate ) THEN
   DO m = 1,sm_levels
     DO n = 1,nsurft
 
-      !Set the current soil tile (see notice above)
-      IF (nsoilt == 1) THEN
-        !There is only 1 soil tile
-        mm = 1
+     IF ( .NOT. ((l_flake_model) .AND. (n == lake)) ) THEN
+       !Set the current soil tile (see notice above)
+       IF (nsoilt == 1) THEN
+         !There is only 1 soil tile
+         mm = 1
 !$OMP DO SCHEDULE(STATIC)
-        DO k = 1,surft_pts(n)
-          l = surft_index(k,n)
-          ext_soilt(l,mm,m) = frac_irr_soilt(l,mm) * ext_irr_soilt(l,mm,m) +   &
-               (1. - frac_irr_soilt(l,mm)) * ext_nir_soilt(l,mm,m)
-        END DO !surft_pts
+         DO k = 1,surft_pts(n)
+           l = surft_index(k,n)
+           ext_soilt(l,mm,m) = frac_irr_soilt(l,mm) * ext_irr_soilt(l,mm,m) +  &
+                (1. - frac_irr_soilt(l,mm)) * ext_nir_soilt(l,mm,m)
+         END DO !surft_pts
 !$OMP END DO
 
-      ELSE ! nsoilt == nsurft
-        !Soil tiles map directly on to surface tiles
-        mm = n
+       ELSE ! nsoilt == nsurft
+         !Soil tiles map directly on to surface tiles
+         mm = n
 !$OMP DO SCHEDULE(STATIC)
-        DO k = 1,surft_pts(n)
-          l = surft_index(k,n)
-          ext_soilt(l,mm,m) = frac_irr_soilt(l,mm) * ext_irr_soilt(l,mm,m) +   &
-               (1. - frac_irr_soilt(l,mm)) * ext_nir_soilt(l,mm,m)
-        END DO !surft_pts
+         DO k = 1,surft_pts(n)
+           l = surft_index(k,n)
+           ext_soilt(l,mm,m) = frac_irr_soilt(l,mm) * ext_irr_soilt(l,mm,m) +  &
+                (1. - frac_irr_soilt(l,mm)) * ext_nir_soilt(l,mm,m)
+         END DO !surft_pts
 !$OMP END DO
-      END IF !nsoilt
-
+       END IF !nsoilt
+     END IF 
     END DO !nsurft
   END DO !sm_levels
 END IF !l_soil_evap_irrig_separate
