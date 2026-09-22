627f063d4cb533c6db3a9308ac28aed7fa9afcb9
Arthur Argles <159253998+mo-arthurargles@users.noreply.github.com>
2026-09-22 16:28:58 +0100

    Update src/control/shared/veg3_field_mod.F90
    Co-authored-by: mo-eddy <156211690+mo-eddy@users.noreply.github.com>

--- src/control/shared/veg3_field_mod.F90
+++ src/control/shared/veg3_field_mod.F90
@@ -582,7 +582,7 @@ END DO
 
 !Final aggregation to gridbox for vegetation carbon for diagnostic purposes
 veg_state%vegC = pfttiles_to_gbm(veg_state%vegCpft,ainfo,frac_surft_in         &
-               = veg_state%frac)
+                              frac_surft_in = veg_state%frac)
 
 ! Aggregate the per-PFT litter contributions for the gridbox total
 veg_state%litC(:) = pfttiles_to_gbm(veg_state%litCpft,ainfo,frac_surft_in      &
