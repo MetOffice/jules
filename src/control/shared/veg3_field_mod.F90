2afc95474141b769f4fef6812560fd356e711b93
mo-arthurargles <arthur.argles@metoffice.gov.uk>
2026-09-23 15:51:22 +0100

    Fixing a typo.

--- src/control/shared/veg3_field_mod.F90
+++ src/control/shared/veg3_field_mod.F90
@@ -581,12 +581,12 @@ DO n = 1,nnpft
 END DO
 
 !Final aggregation to gridbox for vegetation carbon for diagnostic purposes
-veg_state%vegC = pfttiles_to_gbm(veg_state%vegCpft,ainfo,frac_surft_in         &
+veg_state%vegC = pfttiles_to_gbm(veg_state%vegCpft,ainfo,                      &
                               frac_surft_in = veg_state%frac)
 
 ! Aggregate the per-PFT litter contributions for the gridbox total
-veg_state%litC(:) = pfttiles_to_gbm(veg_state%litCpft,ainfo,frac_surft_in      &
-                  = veg_state%frac)
+veg_state%litC(:) = pfttiles_to_gbm(veg_state%litCpft,ainfo,                   &
+                  frac_surft_in = veg_state%frac)
 
 RETURN
 END SUBROUTINE red_veg3_couple
